236d0ec63fca041c1addba5e4f0afcf06289a805
maryam281 <maryamch281@gmail.com>
2021-01-08 22:50:20 -0700

    splash screen added

--- lib/screens.dart
+++ lib/screens.dart
@@ -1,4 +1,5 @@
 export 'screen/home_screen.dart';
 export 'screen/login_screen.dart';
 export 'screen/note_editor.dart';
-export 'screen/settings_screen.dart';
+export 'splash screen/settings_screen.dart';
+
